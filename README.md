ef78f04bd03d2cb2d4bf00b93c3293d946ea7425
dengzhifeng <398577351@qq.com>
2020-12-04 17:27:51 +0800

    update: 区分github 和 个人站点配置

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-
 ## 快速开始
-### 安装主题
-1. 在Hexo目录下执行
-git clone https://github.com/fi3ework/hexo-theme-archer.git themes/archer --depth=1
 
-2. 修改Hexo目录下的 _config.yml 的 theme 字段为 archer
-3. 添加sidebar启用支持：
-在Hexo目录下的 _config.yml 中添加以下字段（不是archer下的 _config.yml）
+### 安装主题
+
+1. 在 Hexo 目录下执行
+   git clone https://github.com/fi3ework/hexo-theme-archer.git themes/archer --depth=1
+
+2. 修改 Hexo 目录下的 \_config.yml 的 theme 字段为 archer
+3. 添加 sidebar 启用支持：
+   在 Hexo 目录下的 \_config.yml 中添加以下字段（不是 archer 下的 \_config.yml）
+
 ```
 jsonContent:
   meta: true
@@ -31,7 +33,7 @@ jsonContent:
 
 ### 创建一个博客
 
-``` bash
+```bash
 $ hexo new "My New Post"
 ```
 
@@ -39,7 +41,7 @@ More info: [Writing](https://hexo.io/docs/writing.html)
 
 ### 启动服务器
 
-``` bash
+```bash
 $ hexo server
 或者
 $ npm run server
@@ -49,7 +51,7 @@ More info: [Server](https://hexo.io/docs/server.html)
 
 ### 生成要部署的静态文件
 
-``` bash
+```bash
 $ hexo generate
 或者
 npm run build
@@ -57,17 +59,25 @@ npm run build
 
 More info: [Generating](https://hexo.io/docs/generating.html)
 
+### 部署到 github 站点
 
+直接部署到 github page
 
-
-### 部署到站点
-直接部署到github page
 ```
-$ hexo clean && hexo generate --deploy  
+$ hexo clean && hexo generate --deploy
 ```
-或者 
+
+或者
+
 ```
 $ npm run deploy
+
+### 部署到个人站点
+```
+
+$ build:mysite 打包 手动放到服务器位置 /home/blog (有空搞搞自动化部署)
+
 ```
 
 More info: [Deployment](https://hexo.io/docs/one-command-deployment.html)
+```
---
+++ replace.js
@@ -0,0 +1,123 @@
+#!/usr/bin/env node
+
+/**
+ * Module dependencies.
+ */
+const { Command } = require("commander");
+const program = new Command();
+const cp = require("child_process");
+const util = require("util");
+const exec = util.promisify(cp.exec);
+const path = require("path");
+// const chalk = require("chalk");
+// const inquirer = require("inquirer");
+const { version } = require("./package.json");
+// const {
+//     format_time,
+//     copyToClipboard,
+//     successLog,
+//     errorLog,
+//     log
+// } = require('./utils.js')
+
+let environmental = "test",
+  name = "";
+let packageJson = {};
+
+function resolve(pathname) {
+  return path.resolve(process.cwd(), pathname);
+}
+
+try {
+  packageJson = require(resolve("package.json"));
+  name = packageJson.name;
+} catch (error) {}
+
+program
+  .version(`v${version}`)
+  .description(`这是一个命令行工具 版本v${version}`)
+  .option("-file,--file [name]", "send your file")
+  .option("-newfile,--newfile [name]", "send your newfile");
+
+program.on("--help", function () {
+  console.log("");
+  console.log("Examples:");
+  console.log("  $ custom-help --help");
+  console.log("  $ custom-help -h");
+});
+
+//替换文件
+// error on unknown commands
+program
+  .command("auto-replace-file")
+  .description("run auto-replace-file for repalce file")
+  .action(async function (env, options) {
+    try {
+      const file = program.file || "package.json";
+      const newfile = program.newfile || "rf-package.json";
+      await exec(`cp -f ${resolve(newfile)} ${resolve(file)}`);
+      console.log("配置文件替换");
+    } catch (error) {
+      console.log("command push---->", error);
+    }
+  });
+
+//构建项目
+// error on unknown commands
+// program
+//     .command('push [env]')
+//     .option('-m, --message <message>', 'replace file')
+//     .option('-r, --replace <replace>', 'replace file')
+//     .option('-p, --push <push>', 'push file')
+//     .description('run push commands for all envs')
+//     .action(async function (env, options) {
+//         try {
+//             const replace = options.replace;
+//             const push = options.push;
+//             const file = program.file || 'package.json';
+//             const newfile = program.newfile || 'rf-package.json';
+//             if (replace) {
+//                 await exec(`cp -f ${resolve(newfile)} ${resolve(file)}`);
+//                 console.log('配置文件替换');
+//             }
+//             console.log('代码提交中···')
+//             const tagName = `${format_time(new Date().getTime(), 'yyyy-MM-dd-hh:mm:ss')}`;
+//             const message = options.message || `feat:${tagName}`
+//             await exec(`git init`)
+//             await exec(`git add .`)
+//             await exec(`git stash`)
+//             await exec(`git pull origin`)
+//             await exec(`git stash pop`)
+//             await exec(`git add .`)
+//             await exec(`git commit -m ${message}`).catch(error => {
+//                 console.log('commit---->', error)
+//             })
+//             await exec(`git push origin`)
+//             console.log('代码提交成功')
+//         } catch (error) {
+//             console.log('command push---->', error)
+//         }
+//     })
+
+//serve
+// error on unknown commands
+//配置启动一个静态资源服务器
+program
+  .command("auto-serve [env]")
+  .description("run auto-serve commands for all envs")
+  .option("-p, --port <port>", "Which exec port to use")
+  .option("-d, --dir <dir>", "Which exec dir to use")
+  .action(async function (env, options) {
+    try {
+      const port = options.port || "8009";
+      const dir = options.dir || "";
+      // console.log('options---->', options, env);
+      console.log("exc---->", `anywhere -p ${port} -d ${dir}`);
+      await exec(`anywhere -p ${port} -d ${dir}`);
+    } catch (error) {
+      console.log("auto-serve anywhere---->", error);
+      console.log("如未安装anywhere请自行安装 npm i anywhere -g");
+    }
+  });
+
+program.parse(process.argv);
